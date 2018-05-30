5a6b71655de59ddc977e23b64736748f08a60a86
Вигура Виктор <v.vihura@mstsoft.org>
2018-05-30 17:29:18 +0300

    Add config files before create gitignor

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://lh/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://lh/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/OSPanel/domains/lh/catalog/');
+define('DIR_SYSTEM', 'D:/OSPanel/domains/lh/system/');
+define('DIR_IMAGE', 'D:/OSPanel/domains/lh/image/');
+define('DIR_LANGUAGE', 'D:/OSPanel/domains/lh/catalog/language/');
+define('DIR_TEMPLATE', 'D:/OSPanel/domains/lh/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/OSPanel/domains/lh/system/config/');
+define('DIR_CACHE', 'D:/OSPanel/domains/lh/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/OSPanel/domains/lh/system/storage/download/');
+define('DIR_LOGS', 'D:/OSPanel/domains/lh/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/OSPanel/domains/lh/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/OSPanel/domains/lh/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'lh');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
